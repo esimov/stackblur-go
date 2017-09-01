2cb0582cbcc16e9cdc94eeb6d28c8e0f538ccd53
esimov <esimovaq@yahoo.com>
2017-09-01 12:16:42 +0300

    Implementing algorithm & add example

---
+++ example/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"os"
+	"log"
+	"time"
+	"fmt"
+	"flag"
+	"image"
+	"image/png"
+	_ "image/png"
+	_ "image/jpeg"
+	"github.com/esimov/stackblur-go"
+)
+
+var (
+	source		= flag.String("in", "", "Source")
+	destination 	= flag.String("out", "", "Destination")
+)
+
+func main() {
+		flag.Parse()
+
+		img, err := os.Open(*source)
+		defer img.Close()
+
+		src, _, err := image.Decode(img)
+		if err != nil {
+			panic(err)
+		}
+		start := time.Now()
+		dst := stackblur.Process(src, uint32(src.Bounds().Dx()), uint32(src.Bounds().Dy()), 20)
+		end := time.Since(start)		
+		fmt.Printf("Generated in: %.2fs\n", end.Seconds())
+
+		fq, err := os.Create(*destination)
+		defer fq.Close()
+
+		if err = png.Encode(fq, dst); err != nil {
+			log.Fatal(err)
+		}
+
+}
